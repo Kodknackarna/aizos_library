fd56d90980d8e1c90ee49fbf4605c79d274107f4
Adam <Adam.sturesson@live.se>
2024-10-09 13:04:16 +0200

    Added function in LibraryUI to test mock

--- src/main/java/org/example/LibraryUI.java
+++ src/main/java/org/example/LibraryUI.java
@@ -1,4 +1,15 @@
 package org.example;
 
 public class LibraryUI {
+
+    private Library library;
+
+    public LibraryUI(Library library) {
+        this.library = library;
+    }
+
+    public String getBookTitleByIsbn(String isbn) {
+        Book book = library.getBook(isbn);
+        return book.getTitle();
+    }
 }
